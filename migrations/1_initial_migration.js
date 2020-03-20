51be36ec47833c6b303f8ecdb3b615576cf2dc4f
luc1dLife <luc1d_life@protonmail.com>
2020-03-20 15:17:35 -0600

    re-built

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,13 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 const waviii = artifacts.require("waviii");
 
-module.exports = async function(deployer) {
+module.exports = function(deployer) {
   await deployer.deploy(Migrations);
   await deployer.deploy(waviii);
-  const token = await waviii.deployed()
-  // Mint ALL waviii Tokens for admin / Lucid
-  await token.mint(
-    '0x820dF1087EbB9bb1510445bB86C732707a290BD5',
-    '1000000000000000000000000'
-  )
 };
