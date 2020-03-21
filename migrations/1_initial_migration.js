f58cd76b5a179678dc098a192b1fbea2f81e01cd
luc1dLife <luc1d_life@protonmail.com>
2020-03-20 19:00:31 -0600

    updated

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,7 +1,8 @@
 const Migrations = artifacts.require("Migrations");
 const waviii = artifacts.require("waviii");
+const initialSupply = '1000000000000000000000000' // 1 million tokens - use your actual initial supply here
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   await deployer.deploy(Migrations);
-  await deployer.deploy(waviii);
+  await deployer.deploy(waviii, initialSupply);
 };
