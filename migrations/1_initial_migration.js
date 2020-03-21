349f46ab069f1409d717560262aebd85cde70a64
luc1dLife <luc1d_life@protonmail.com>
2020-03-21 13:34:33 -0600

    updated

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -4,10 +4,10 @@ const waviii = artifacts.require("waviii");
 module.exports = async function(deployer) {
   await deployer.deploy(Migrations);
   await deployer.deploy(waviii);
-  const token = await waviii.deployed()
-  // Mint ALL waviii Tokens for admin / Lucid
-   await token.mint(
-   '0x9dB16e30c7AEa07baC4CEc61aDe6DbE9854D732E',
-   '1000000000000000000000000'
+  const erc20token = await waviii.deployed()
+  // Mint 1,000 waviii Tokens for the deployer
+  await erc20token.mint(
+    '0x9dB16e30c7AEa07baC4CEc61aDe6DbE9854D732E',
+    '1000000000000000000000'
   )
 };
--- test/waviii.erc20.js
+++ test/waviii.erc20.js
@@ -23,10 +23,10 @@ contract('waviii', function(accounts) {
       tokenInstance = instance;
       return tokenInstance.totalSupply();
     }).then(function(totalSupply) {
-      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+      assert.equal(totalSupply.toString(), 1000000, 'sets the total supply to 1,000,000');
       return tokenInstance.balanceOf(accounts[0]);
     }).then(function(adminBalance) {
-      assert.equal(adminBalance.toNumber(), 1000000, 'it allocates the initial supply to the admin account');
+      assert.equal(adminBalance.toString(), 1000000, 'it allocates the initial supply to the admin account');
     });
   });
 
